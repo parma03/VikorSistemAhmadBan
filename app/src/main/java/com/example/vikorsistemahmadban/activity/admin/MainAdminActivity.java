342c7860ea9ad421ca2388d0a1891ddc77fdabb2
Tri Setiawan <mk.tri.setiawan2@pertamina.com>
2025-06-21 03:42:28 +0700

    new test

--- app/src/main/java/com/example/vikorsistemahmadban/activity/admin/MainAdminActivity.java
+++ app/src/main/java/com/example/vikorsistemahmadban/activity/admin/MainAdminActivity.java
@@ -62,6 +62,7 @@ public class MainAdminActivity extends AppCompatActivity {
             // Intent ke ProfileSettingActivity
             // Intent intent = new Intent(MainAdminActivity.this, ProfileSettingActivity.class);
             // startActivity(intent);
+            // MEMEK
         });
 
         binding.menuLogout.setOnClickListener(v -> {
